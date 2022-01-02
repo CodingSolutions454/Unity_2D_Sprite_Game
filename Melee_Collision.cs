ec38734182ddfc6aa18bece4a1abc9ea0b60faa2
Daniel Greaves <95943574+CodingSolutions454@users.noreply.github.com>
2022-01-02 22:20:04 +0000

    Add files via upload

---
+++ Melee_Collision.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee_Collision : MonoBehaviour
+{
+    // Float refernces
+    [SerializeField]
+    float Speed;
+    [SerializeField]
+    float DestroyThis = 1;
+
+    // Spawn projectile at direction player is facing
+    public void OnSpawn(bool IsFacingLeft)
+    {
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+
+        if (IsFacingLeft)
+        {
+            // Rotate this projectile 180 degrees if facing left
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            rb2d.velocity = new Vector3(-Speed, 0);
+        }
+        else
+        {
+            // Or retain the same rotation if we are not facing left
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            rb2d.velocity = new Vector3(Speed, 0);
+        }
+
+        // destroy this actor after 1 seconds.
+        Destroy(gameObject, DestroyThis);
+    }
+
+}
